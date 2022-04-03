8171d047581dee08c9fea2c9a862264fbfaeed73
gonczyL <95233393+gonczyL@users.noreply.github.com>
2022-04-03 18:31:14 +0200

    SceneController01
    Login hozzadva
    login as guest hozzadva
    Admin bejelntkezes

--- Prison/Prison/src/main/java/inf/unideb/prison/SceneController.java
+++ Prison/Prison/src/main/java/inf/unideb/prison/SceneController.java
@@ -7,6 +7,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import javax.xml.transform.Source;
@@ -24,6 +26,12 @@ public class SceneController {
 
     @FXML
     private Button wardenReportButton;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
 
     @FXML
     void onPrisonerButton(ActionEvent event) {
@@ -55,5 +63,46 @@ public class SceneController {
         }
     }
 
+    @FXML
+    void onLoginGuest(ActionEvent event){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AfterLoginScreen.fxml"));
+            Parent par1 =fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.setTitle("Prisoner");
+            stage.setScene(new Scene(par1));
+            stage.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
+    @FXML
+    void onLogin(ActionEvent event) {
+        try {
+            String usernameinput = username.getText();
+            String passwordinput = password.getText();
+            if (usernameinput.equals("admin") && passwordinput.equals("admin")){
+                try {
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AfterLoginScreen.fxml"));
+                    Parent par1 =fxmlLoader.load();
+                    Stage stage = new Stage();
+                    stage.setResizable(false);
+                    stage.setTitle("Prisoner");
+                    stage.setScene(new Scene(par1));
+                    stage.show();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            } else{
+                System.out.println("rossz vagy");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+    }
 }

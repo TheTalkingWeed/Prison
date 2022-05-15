e573ccd2082fe1c7b4b3d215a44ae8298edd0997
gonczyL <95233393+gonczyL@users.noreply.github.com>
2022-05-15 18:01:50 +0200

    SceneController meglett csinálva, elég rendesen

--- PrisonManagment/src/main/java/hu/unideb/inf/Controller/SceneController.java
+++ PrisonManagment/src/main/java/hu/unideb/inf/Controller/SceneController.java
@@ -1,18 +1,26 @@
 package hu.unideb.inf.Controller;
 
+import hu.unideb.inf.model.Admin;
+import hu.unideb.inf.model.AdminDAO;
+import hu.unideb.inf.model.JpaAdminDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class SceneController {
 
+    private boolean isAdmin;
 
     @FXML
     private TextField username;
@@ -20,11 +28,26 @@ public class SceneController {
     @FXML
     private PasswordField password;
 
+    @FXML
+    private Button loginAsadmin;
 
+    @FXML
+    private Button loginAsguest;
 
     @FXML
     void onLoginGuest(ActionEvent event){
         try {
+            isAdmin = false;
+
+            Alert alertwindow = new Alert(Alert.AlertType.INFORMATION);
+
+            alertwindow.setTitle("Logged in successfully");
+            alertwindow.setContentText("You logged in as Guest!");
+            alertwindow.showAndWait();
+
+            Stage stage2 = (Stage) loginAsguest.getScene().getWindow();
+            stage2.close();
+
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/GUI.fxml"));
             Parent par1 =fxmlLoader.load();
             Stage stage = new Stage();
@@ -32,6 +55,7 @@ public class SceneController {
             stage.setTitle("Prisoner");
             stage.setScene(new Scene(par1));
             stage.show();
+
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -42,8 +66,30 @@ public class SceneController {
         try {
             String usernameinput = username.getText();
             String passwordinput = password.getText();
-            if (usernameinput.equals("admin") && passwordinput.equals("admin")){
+            AdminDAO adminDAO = new JpaAdminDAO();
+            List<Admin> admins = new ArrayList<>(adminDAO.getAdmins());
+
+            if(!containsUsername(admins,usernameinput))
+            {
+                Alert alertwindow = new Alert(Alert.AlertType.WARNING);
+
+                alertwindow.setTitle("Hibás belépés");
+                alertwindow.setContentText("Felhasználó nem található");
+                alertwindow.show();
+
+
+
+            }else if (isValid(admins,usernameinput,passwordinput)){
                 try {
+                    Alert alertwindow = new Alert(Alert.AlertType.INFORMATION);
+
+                    alertwindow.setTitle("Logged in successfully");
+                    alertwindow.setContentText("You logged in as Admin!");
+                    alertwindow.showAndWait();
+
+                    Stage stage2 = (Stage) loginAsadmin.getScene().getWindow();
+                    stage2.close();
+                    isAdmin = true;
                     FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/GUI.fxml"));
                     Parent par1 =fxmlLoader.load();
                     Stage stage = new Stage();
@@ -63,5 +109,28 @@ public class SceneController {
 
 
 
+    }
+
+    private boolean containsUsername(List<Admin> admins, String username){
+        return admins.stream().filter(o -> o.getUsername().equals(username)).findFirst().isPresent();
+    }
+
+    private String getPasswordByUserName(List<Admin> admins, String username) {
+        //return admins.stream().filter(o -> o.getPassword().equals(password)).findFirst().isPresent();
+
+        for (Admin a : admins) {
+            if (a.getUsername().equals(username)) {
+                return a.getPassword();
+            }
+        }
+        return null;
+    }
+    private boolean isValid(List<Admin> admins, String username,String password){
+
+
+
+        if (getPasswordByUserName(admins,username).equals(password)) return true;
+
+        return false;
     }
 }

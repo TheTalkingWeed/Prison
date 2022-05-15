af857712476331b1907895a192519b01e5f9356f
domqq <79380425+domqq@users.noreply.github.com>
2022-05-15 22:02:18 +0200

    GuiController

---
+++ PrisonManagment/src/main/java/hu/unideb/inf/Controller/guiController.java
@@ -0,0 +1,54 @@
+package hu.unideb.inf.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class guiController {
+
+    @FXML
+    private Button deleteButtonMP;
+
+    @FXML
+    private Button editWardenDetailsButton;
+
+    @FXML
+    private TextField prisonerID_textbox;
+
+    @FXML
+    private TextField prisonerName_textbox;
+
+    @FXML
+    private TextField prisonerName_textbox1;
+
+    @FXML
+    private TextField prisonerName_textbox11;
+
+    @FXML
+    private Button saveButtonMP;
+
+    @FXML
+    private Button searchButtonMP;
+
+    @FXML
+    void DeleteButtonMP(ActionEvent event) {
+
+    }
+
+    @FXML
+    void EditWardenDetailsButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SaveButtonMP(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SearchButtonMP(ActionEvent event) {
+
+    }
+
+}

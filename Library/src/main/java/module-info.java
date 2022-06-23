62427e9bd43a402dcc0c578b56d5d526ff8b4176
Alex_sz1 <61713353+TheTalkingWeed@users.noreply.github.com>
2022-06-23 17:20:10 +0200

    Delete Library directory

--- Library/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module hu.alex.library.library {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens hu.alex.library.library to javafx.fxml;
-    exports hu.alex.library.library;
-}
